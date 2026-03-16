6a8fcec9f6b0e13347b9568d5644c260f4fcd9d3
SurajPawarDev15 <supersuraj143@gmail.com>
2026-03-16 12:06:24 +0530

    completed day-02

--- src/App.js
+++ src/App.js
@@ -1,11 +1,54 @@
+import { useState } from "react";
 import Welcome from "./Welcome";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const greeting = "Hello , GM";
+  const members = ["Suraj", "Rahul", "Amit", "Priya"];
+  const students = [
+    { name: "Suraj", age: 20, class: 5, grade: 90 },
+    { name: "Rahul", age: 21, class: 6, grade: 85 },
+    { name: "Priya", age: 19, class: 5, grade: 92 },
+    { name: "Amit", age: 22, class: 7, grade: 88 },
+    { name: "Neha", age: 20, class: 6, grade: 95 },
+  ];
+  const welcomeData = {
+    greeting: greeting,
+    members: members,
+    students: students,
+  };
   return (
-    <div className="App">
+    <>
       <h1>Hello React Welcome to Learning</h1>
-      <Welcome></Welcome>
-    </div>
+      <Welcome {...welcomeData}></Welcome>
+      <div>
+        <h3>Counter Value :{counter}</h3>
+        <button
+          style={{
+            backgroundColor: "#00000",
+            border: "1px solid black",
+            color: "black",
+            margin: "2px",
+            padding: "5px",
+          }}
+          onClick={() => setCounter(counter + 1)}
+        >
+          Increase value
+        </button>
+        <button
+          style={{
+            backgroundColor: "#00000",
+            border: "1px solid black",
+            color: "black",
+            margin: "1px",
+            padding: "5px",
+          }}
+          onClick={() => setCounter(counter - 1)}
+        >
+          Decrease value
+        </button>
+      </div>
+    </>
   );
 }
 
--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -1,13 +1,40 @@
-function Welcome() {
+function Welcome({ greeting, members, students }) {
   const channelName = "ThinkCreateShareGrow";
   const isDisplay = false;
+
+  console.log(members);
+
   return (
     <div>
       <h1>
-        Welcome to my Channel ,
+        Welcome to my Channel,
         {isDisplay ? channelName : " Please subscribe my channel"}
       </h1>
+
+      <p>Greeting from Parent : {greeting}</p>
+
+      <div>
+        <p>Members Are of ReactJS Family</p>
+
+        {members.map((member, index) => (
+          <div key={index}>{member}</div>
+        ))}
+      </div>
+
+      <div>
+        <p>Students of ReactJS Family</p>
+
+        {students.map((student, index) => (
+          <div key={index}>
+            <p>Student Name: {student.name}</p>
+            <p>Student Age: {student.age}</p>
+            <p>Student Class: {student.class}</p>
+            <p>Student Grade: {student.grade}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
+
 export default Welcome;
